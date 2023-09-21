cb1deacb80f85c1ee5e0add6a18abbe9af60af20
hzp <hzhouping@outlook.com>
2023-09-21 15:13:51 +0800

    feat: 配置icon插件

--- packages/vite-config/src/common.js
+++ packages/vite-config/src/common.js
@@ -1,6 +1,10 @@
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import pages from 'vite-plugin-pages'
+import icons from 'unplugin-icons/vite'
+import iconsResolver from 'unplugin-icons/resolver'
+import components from 'unplugin-vue-components/vite'
+import { FileSystemIconLoader } from 'unplugin-icons/loaders'
 
 /** @type {import('vite').UserConfig} */
 const commonConfig = {
@@ -18,6 +22,24 @@ const commonConfig = {
       dirs: 'src/views',
       extensions: ['vue'],
       exclude: ['**/components/**/*', '**/modules/**/*']
+    }),
+    components({
+      dts: false,
+      resolvers: [
+        iconsResolver({
+          componentPrefix: 'icon',
+          customCollections: ['local']
+        })
+      ]
+    }),
+    icons({
+      scale: 1,
+      compiler: 'vue3',
+      customCollections: {
+        local: FileSystemIconLoader('src/assets/icons', svg =>
+          svg.replace(/^<svg\s/, '<svg width="1em" height="1em" ')
+        )
+      }
     })
   ]
 }
